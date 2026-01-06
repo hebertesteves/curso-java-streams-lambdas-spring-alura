c5c8bc5c24dea5a2bb1c6e9d092193bc795ef101
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-01-06 19:47:46 -0300

    feat: cria menu de interecao e busca de dados completos da serie

--- screenmatch/src/main/java/hebertesteves/screenmatch/ScreenmatchApplication.java
+++ screenmatch/src/main/java/hebertesteves/screenmatch/ScreenmatchApplication.java
@@ -1,16 +1,9 @@
 package hebertesteves.screenmatch;
 
-import hebertesteves.screenmatch.model.DadosEpisodio;
-import hebertesteves.screenmatch.model.DadosSerie;
-import hebertesteves.screenmatch.model.DadosTemporada;
-import hebertesteves.screenmatch.service.ConsumoApi;
-import hebertesteves.screenmatch.service.ConverteDados;
+import hebertesteves.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -21,35 +14,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
-		// System.out.println(json);
-		// json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
-		System.out.println(json);
-
-		System.out.println();
-
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
-
-		System.out.println();
-
-		json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=6585022c");
-		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
-		System.out.println(dadosEpisodio);
-
-		System.out.println();
-
-		List<DadosTemporada> temporadas = new ArrayList<>();
-
-		for (int i = 1; i <= dados.totalTemporadas(); i++) {
-			json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=6585022c");
-			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-			temporadas.add(dadosTemporada);
-		}
-
-		temporadas.forEach(System.out::println);
-
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 }
---
+++ screenmatch/src/main/java/hebertesteves/screenmatch/principal/Principal.java
@@ -0,0 +1,38 @@
+package hebertesteves.screenmatch.principal;
+
+import hebertesteves.screenmatch.model.DadosSerie;
+import hebertesteves.screenmatch.model.DadosTemporada;
+import hebertesteves.screenmatch.service.ConsumoApi;
+import hebertesteves.screenmatch.service.ConverteDados;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal {
+    private Scanner sc = new Scanner(System.in);
+    private final String ENDERECO = "https://www.omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=6585022c";
+    private final String SEASON = "&season=";
+    private ConsumoApi consumoApi = new ConsumoApi();
+    private ConverteDados conversor = new ConverteDados();
+
+    public void exibeMenu() {
+        System.out.print("Digite o nome da série para busca: ");
+        var nomeSerie = sc.nextLine();
+        var json = consumoApi.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
+
+        List<DadosTemporada> temporadas = new ArrayList<>();
+
+		for (int i = 1; i <= dados.totalTemporadas(); i++) {
+			json = consumoApi.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + SEASON + i + API_KEY);
+			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+
+		temporadas.forEach(System.out::println);
+    }
+}
