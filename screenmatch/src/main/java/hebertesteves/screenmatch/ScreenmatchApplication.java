a3010beb7bd52f7269c7bc19bb0e664a3ba22c2a
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-01-05 22:03:04 -0300

    feat: modelando temporadas

--- screenmatch/src/main/java/hebertesteves/screenmatch/ScreenmatchApplication.java
+++ screenmatch/src/main/java/hebertesteves/screenmatch/ScreenmatchApplication.java
@@ -2,11 +2,15 @@ package hebertesteves.screenmatch;
 
 import hebertesteves.screenmatch.model.DadosEpisodio;
 import hebertesteves.screenmatch.model.DadosSerie;
+import hebertesteves.screenmatch.model.DadosTemporada;
 import hebertesteves.screenmatch.service.ConsumoApi;
 import hebertesteves.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -34,5 +38,18 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=6585022c");
 		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
 		System.out.println(dadosEpisodio);
+
+		System.out.println();
+
+		List<DadosTemporada> temporadas = new ArrayList<>();
+
+		for (int i = 1; i <= dados.totalTemporadas(); i++) {
+			json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=6585022c");
+			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+
+		temporadas.forEach(System.out::println);
+
 	}
 }
